28cd718bb5212a5ca2cf5aef1afe9b131401b2e0
unknown <158172601@qq.com>
2025-05-14 22:16:31 +0800

    优化

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from "@/lib/widgets";
-export * from "@/lib/enums";
-export * from "@/lib/class";
-export * from "@/lib/interface";
+export * from "./lib/widgets"
+export * from "./lib/enums";
+export * from "./lib/class";
+export * from "./lib/interface";
 
 export { runApp } from "gen-ui";
